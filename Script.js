c6b9049f3e32665a8ce679cb99b2f9417f5a3714
Christiane Silva <la_christianesilva@la-christianesilva.local>
2019-03-26 15:08:39 -0300

    Refatoração e ajuste do modal para ser responsivo

--- Script.js
+++ Script.js
@@ -1,26 +1,17 @@
+var model = document.getElementById('janela1');
+var buttonTalkWithUs = document.getElementById("myBtn");
+var spanClose = document.getElementsByClassName("close")[0];
 
-// Get the modal
-var button = document.getElementById('janela1');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  button.style.display = "block";
+buttonTalkWithUs.onclick = function() {
+    model.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  button.style.display = "none";
+spanClose.onclick = function() {
+    model.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == button) {
-    button.style.display = "none";
-  }
+    if (event.target == model) {
+        model.style.display = "none";
+    }
 }
