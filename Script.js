38e9ef46d3a0579633ee4f858a6bd6ccb09268e5
Akin Abaz <akinabaz@gmail.com>
2019-03-27 17:31:22 -0300

    form do projeto de doação de notes

--- Script.js
+++ Script.js
@@ -1,17 +1,31 @@
-var model = document.getElementById('janela1');
+var modelTalkWithUs = document.getElementById('janela1');
 var buttonTalkWithUs = document.getElementById("myBtn");
-var spanClose = document.getElementsByClassName("close")[0];
+var spanCloseTalk = document.getElementsByClassName("close")[0];
+
+var modelForm = document.getElementById('janela2');
+var buttonSubscribe = document.getElementById("myBtnSubs");
+var spanCloseSubs = document.getElementById("closeSubs");
 
 buttonTalkWithUs.onclick = function() {
-    model.style.display = "block";
+    modelTalkWithUs.style.display = "block";
 }
 
-spanClose.onclick = function() {
-    model.style.display = "none";
+spanCloseTalk.onclick = function() {
+    modelTalkWithUs.style.display = "none";
 }
 
 window.onclick = function(event) {
-    if (event.target == model) {
-        model.style.display = "none";
+    if (event.target == modelTalkWithUs) {
+        modelTalkWithUs.style.display = "none";
+    } else if (event.target == modelForm) {
+        modelForm.style.display = "none";
     }
+}
+
+buttonSubscribe.onclick = function() {
+    modelForm.style.display = "block";
+}
+
+spanCloseSubs.onclick = function() {
+    modelForm.style.display = "none";
 }
